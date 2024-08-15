8b28644107c0fed7a1fe81a3d6880abd689afd00
Pedronovaes1 <pedro.novaes@souunit.com.br>
2024-08-15 14:08:24 -0300

    adicionando métodos de remoção, adição e mostrar livros

--- javinhas/src/gerenciamentoBiblioteca/Biblioteca.java
+++ javinhas/src/gerenciamentoBiblioteca/Biblioteca.java
@@ -1,4 +1,43 @@
 package gerenciamentoBiblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+    //atributos
+    private List<Livro> livros;
+    private List<Usuario> usuarios;
+
+    public Biblioteca() {
+        this.usuarios = new ArrayList<>();
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(int id,String titulo, String autor, String editora){
+            Livro livro = new Livro(id,titulo, autor, editora);
+            this.livros.add(livro);
+    }
+
+    public void removerLivro(int id){
+        List<Livro> livrosParaRemover = new ArrayList<>();
+        if(!this.livros.isEmpty()){
+            for(Livro livro: this.livros){
+                if(livro.getId() == id){
+                    livrosParaRemover.add(livro);
+                }
+            }
+            this.livros.removeAll(livrosParaRemover);
+        }else{
+            System.out.println("A lista está vazia");
+        }
+    }
+
+    public void mostrarLivros() {
+        if(!livros.isEmpty()) {
+            System.out.println(livros);
+        }else{
+            System.out.println("A lista está vazia!!");
+        }
+    }
+
 }
--- javinhas/src/gerenciamentoBiblioteca/Livro.java
+++ javinhas/src/gerenciamentoBiblioteca/Livro.java
@@ -3,10 +3,56 @@ package gerenciamentoBiblioteca;
 public class Livro {
 
     //atributos
-    private String isbn;
+    private int id;
     private String titulo;
     private String autor;
     private String editora;
 
+    public Livro(int id,String titulo, String autor, String editora) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editora='" + editora + '\'' +
+                '}';
+    }
 }
