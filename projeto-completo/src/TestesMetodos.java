7ed6f79876bbf7eccee775d0a83416ca5e372bb2
Pedronovaes1 <pedro.novaes@souunit.com.br>
2024-08-21 22:24:57 -0300

    projeto completo

--- projeto-completo/src/TestesMetodos.java
+++ projeto-completo/src/TestesMetodos.java
@@ -1,6 +1,6 @@
 import gerenciamentoBiblioteca.Recepcao;
 
-public class TestesMétodos {
+public class TestesMetodos {
     public static void main(String[] args) {
         Recepcao recepcao = new Recepcao();
         recepcao.iniciar();
